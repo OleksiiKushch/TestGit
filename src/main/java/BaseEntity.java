31b74d3717ad516c7a291b4ea1b665f743b4cf1e
OleksiiKushch <58341654+OleksiiKushch@users.noreply.github.com>
2022-01-29 19:41:16 +0200

    add BaseEntity class

---
+++ src/main/java/BaseEntity.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    public BaseEntity() {}
+
+    public static void foo() {
+
+    }
+
+}
