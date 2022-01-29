ed57ed62c13d6e2410f2ab3f56459ae14151290c
OleksiiKushch <58341654+OleksiiKushch@users.noreply.github.com>
2022-01-29 22:39:45 +0200

    impl method bookFoo

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,7 +1,14 @@
 public class Book extends BaseEntity {
 
     public static void bookFoo() {
+        for(int j = 0; j < 10; j++) {
+            if (j != 10 - 1) {
+                System.out.print(0);
+            } else {
+                System.out.print(1);
+            }
 
+        }
     }
 
 }
